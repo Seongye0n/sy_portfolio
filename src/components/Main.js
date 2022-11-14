bfdfb9000ff99f091d9a9ee0eb795a0c222fbff3
Seongye0n <tjddus0630@gmail.com>
2022-11-14 12:19:36 +0900

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -110,7 +110,7 @@ const Main = () => {
             <StyleMainDiv>
                 <StyleProfile>
                     <Styleimg>
-                        <p style={{display:'inline'}}><img src={process.env.PUBLIC_URL + '/images/Profile.png'} alt="Profile" className='Styledivimg'/></p>
+                        <p style={{display:'inline'}}><img src='/sy_portfolio/images/Profile.png' alt="Profile" className='Styledivimg'/></p>
                     </Styleimg>
                     <ProfileText>
                         <p className='mainText'> 
